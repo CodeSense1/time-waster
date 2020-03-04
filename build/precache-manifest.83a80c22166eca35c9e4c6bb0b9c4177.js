4fb7e866f14fe742ce7fc258ac3aa8cbc7d46793
Joonas Kakkuri <joonas.kakkuri@gmail.com>
2020-03-04 15:32:34 +0200

    lisätty oikeesti se eka build

---
+++ build/precache-manifest.83a80c22166eca35c9e4c6bb0b9c4177.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8b9b563dd16fe719cbfa6b6337515b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34d471370ab79932a2fb",
+    "url": "/static/css/main.da8495a5.chunk.css"
+  },
+  {
+    "revision": "e4878c09dcd73e91dded",
+    "url": "/static/js/2.7eb117fe.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.7eb117fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34d471370ab79932a2fb",
+    "url": "/static/js/main.22ffcdd5.chunk.js"
+  },
+  {
+    "revision": "51ad1dad18b2fe074e59",
+    "url": "/static/js/runtime-main.30d61526.js"
+  }
+]);
